1d99f37ffe49f8a8322606d6032111bff24e2ca0
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2020-06-07 20:33:03 +1200

    El Sombrero example site - add gallery page

---
+++ el-sombrero/gallery.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>El Sombrero Restaurant - Gallery</title>
+  </head>
+  <body>
+    <!-- include banner  -->
+    <?php include('banner.php'); ?>
+
+    <!-- main content -->
+    <div class="gallery-container">
+      <div class="gallery-image">
+        <img src="images/burrito.jpg" alt="burrito">
+      </div>
+      <div class="gallery-image">
+        <img src="images/chillis.jpg" alt="chills">
+      </div>
+      <div class="gallery-image">
+        <img src="images/gucamole.jpg" alt="gucamole">
+      </div>
+      <div class="gallery-image">
+        <img src="images/platter.png" alt="platter">
+      </div>
+      <div class="gallery-image">
+        <img src="images/tortilla.jpg" alt="tortilla">
+      </div>
+
+    </div> <!-- gallery-container -->
+
+
+    <!-- include footer -->
+    <?php include('footer.php'); ?>
+
+  </body>
+</html>
