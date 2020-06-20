00be1b0e0b807a8290fa492d7677756ded16ace0
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2020-06-21 11:54:46 +1200

    Code snippets for teaching

--- student-demo/snippets.php
+++ student-demo/snippets.php
@@ -1,14 +1,4 @@
 <?php
-// display results of query
-do {
 
-  // display each row of results
-  // student details
-  echo "    <p>Student ID: " . $student_aa['student_id'] . "</p>\n";
-  echo "    <p class='firstname'>" . $student_aa['first_name'] . "</p>\n";
-  echo "    <p class='lastname'>" . $student_aa['last_name'] . "</p>\n";
-
-
-} while ($student_aa = mysqli_fetch_assoc($student_qry));
 
  ?>
